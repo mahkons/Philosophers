4aac89fa9c0de99ae2a507809975f1cffed0e6cb
Makhnev Konstantin <mah.kons@gmail.com>
2021-04-08 22:34:31 +0300

    add philosophers

---
+++ PPO.py
@@ -0,0 +1,115 @@
+import torch
+import numpy as np
+import itertools
+
+from networks import Actor, Critic
+from params import LR, GAMMA, LAMBDA, CLIP, ENTROPY_COEF, VALUE_COEFF, BATCHES_PER_UPDATE, BATCH_SIZE
+
+
+P_COUNT = 2
+P_DELAY = 1
+
+P_LR = 1e-4
+
+class PPO():
+    def __init__(self, state_dim, action_dim, device):
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.device = device
+
+        self.actor = Actor(state_dim, action_dim).to(device)
+        self.critic = Critic(state_dim).to(device)
+        self.optimizer = torch.optim.Adam(itertools.chain(self.actor.parameters(), self.critic.parameters()), LR)
+
+
+        self.philosophers = list()
+        for i in range(P_COUNT):
+            self.philosophers.append(Critic(state_dim).to(device))
+
+        self.p_optimizers = [torch.optim.Adam(p.parameters(), lr=P_LR) for p in self.philosophers]
+        self.update_cnt = 0
+
+    def _calc_loss(self, state, action, old_log_prob, expected_values, gae):
+        new_log_prob, action_distr = self.actor.compute_proba(state, action)
+        state_values = self.critic.get_value(state).squeeze(1)
+
+        critic_loss = ((expected_values - state_values) ** 2).mean()
+
+        unclipped_ratio = torch.exp(new_log_prob - old_log_prob)
+        clipped_ratio = torch.clamp(unclipped_ratio, 1 - CLIP, 1 + CLIP)
+        actor_loss = -torch.min(clipped_ratio * gae, unclipped_ratio * gae).mean()
+
+        entropy_loss = -action_distr.entropy().mean()
+
+        p_loss = 0
+        for p in self.philosophers():
+            p_state_values = self.critic.get_value(state).squeeze(1)
+            p_loss += ((p_state_values - state_values.detach()) ** 2).mean()
+
+        return critic_loss * VALUE_COEFF + actor_loss + entropy_loss * ENTROPY_COEF + p_loss
+
+
+    def update(self, trajectories):
+        trajectories = map(self._compute_lambda_returns_and_gae, trajectories)
+        transitions = sum(trajectories, []) # Turn a list of trajectories into list of transitions
+
+        state, action, old_log_prob, target_value, advantage = zip(*transitions)
+        state = torch.from_numpy(np.array(state)).float().to(self.device)
+        action = torch.from_numpy(np.array(action)).float().to(self.device)
+        old_log_prob = torch.from_numpy(np.array(old_log_prob)).float().to(self.device)
+        target_value = torch.from_numpy(np.array(target_value)).float().to(self.device)
+        advantage = torch.from_numpy(np.array(advantage)).float().to(self.device)
+
+        for _ in range(BATCHES_PER_UPDATE):
+            idx = np.random.randint(0, len(transitions), BATCH_SIZE)
+            loss = self._calc_loss(state[idx], action[idx], old_log_prob[idx], target_value[idx], advantage[idx])
+
+            self.optimizer.zero_grad()
+            for p_optimizer in self.p_optimizers:
+                p_optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+            for p_optimizer in self.p_optimizers:
+                p_optimizer.step()
+
+        self.update_cnt += 1
+        if self.update_cnt % P_DELAY == 0:
+            self.critic = self.philosophers[0]
+            self.philosophers.pop(0)
+            self.philosophers.append(Critic(self.state_dim).to(self.device))
+            self.p_optimizers.pop(0)
+            self.p_optimizers.append(torch.optim.Adam(self.philosophers[-1].parameters(), lr=P_LR))
+
+
+    def _compute_lambda_returns_and_gae(self, trajectory):
+        lambda_returns = []
+        gae = []
+        last_lr = 0.
+        last_v = 0.
+        for s, _, r, _ in reversed(trajectory):
+            ret = r + GAMMA * (last_v * (1 - LAMBDA) + last_lr * LAMBDA)
+            last_lr = ret
+            last_v = self.get_value(s)
+            lambda_returns.append(last_lr)
+            gae.append(last_lr - last_v)
+        
+        # Each transition contains state, action, old action probability, value estimation and advantage estimation
+        return [(s, a, p, v, adv) for (s, a, _, p), v, adv in zip(trajectory, reversed(lambda_returns), reversed(gae))]
+            
+            
+    def get_value(self, state):
+        with torch.no_grad():
+            state = torch.from_numpy(state).float().unsqueeze(0).to(self.device)
+            value = self.critic.get_value(state)
+        return value.cpu().item()
+
+    def act(self, state):
+        with torch.no_grad():
+            state = torch.from_numpy(state).float().unsqueeze(0).to(self.device)
+            action, pure_action, log_prob = self.actor.act(state)
+        return action.cpu().numpy()[0], pure_action.cpu().numpy()[0], log_prob.cpu().item()
+
+    def save(self):
+        torch.save(self.actor, "agent.pkl")
+
+
---
+++ train.py
@@ -0,0 +1,82 @@
+import pybullet_envs
+# Don't forget to install PyBullet!
+
+import gym
+import numpy as np
+import torch
+from torch import nn
+import random
+
+from PPO import PPO
+from params import  ENV_NAME, MIN_EPISODES_PER_UPDATE, MIN_TRANSITIONS_PER_UPDATE, ITERATIONS, RANDOM_SEED
+
+device = torch.device("cuda")
+
+def evaluate_policy(env, agent, episodes):
+    returns = []
+    for _ in range(episodes):
+        done = False
+        state = env.reset()
+        total_reward = 0.
+        
+        while not done:
+            state, reward, done, _ = env.step(agent.act(state)[0])
+            total_reward += reward
+        returns.append(total_reward)
+    return returns
+   
+
+def sample_episode(env, agent):
+    s = env.reset()
+    d = False
+    trajectory = []
+    while not d:
+        a, pa, logprob = agent.act(s)
+        ns, r, d, _ = env.step(a)
+        trajectory.append((s, pa, r, logprob))
+        s = ns
+    return trajectory
+
+
+def train():
+    env = gym.make(ENV_NAME)
+    env.seed(RANDOM_SEED)
+
+    ppo = PPO(state_dim=env.observation_space.shape[0], action_dim=env.action_space.shape[0], device=device)
+    state = env.reset()
+    episodes_sampled = 0
+    steps_sampled = 0
+
+    best = 0
+    
+    for i in range(ITERATIONS):
+        trajectories = []
+        steps_cnt = 0
+        
+        while len(trajectories) < MIN_EPISODES_PER_UPDATE or steps_cnt < MIN_TRANSITIONS_PER_UPDATE:
+            traj = sample_episode(env, ppo)
+            steps_cnt += len(traj)
+            trajectories.append(traj)
+        episodes_sampled += len(trajectories)
+        steps_sampled += steps_cnt
+
+        ppo.update(trajectories)        
+        
+        if (i + 1) % (ITERATIONS//100) == 0:
+            rewards = evaluate_policy(env, ppo, 20)
+            print(f"Step: {i+1}, Reward mean: {np.mean(rewards)}, Reward std: {np.std(rewards)}, Episodes: {episodes_sampled}, Steps: {steps_sampled}")
+            val = np.mean(rewards)
+            if val > best:
+                best = val
+                ppo.save()
+
+
+def init_random_seeds(RANDOM_SEED):
+    torch.manual_seed(RANDOM_SEED)
+    np.random.seed(RANDOM_SEED)
+    random.seed(RANDOM_SEED)
+
+
+if __name__ == "__main__":
+    init_random_seeds(RANDOM_SEED)
+    train()
